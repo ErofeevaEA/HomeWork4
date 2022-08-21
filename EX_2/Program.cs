03c3acdd5e928b64ff36e95a307e60a0efff38fc
ErofeevaEvgeniya <golubenko.ewgenia@yandex.ru>
2022-08-21 15:24:06 +0300

    Exercise 2

---
+++ EX_2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите целое число:");
+        int x = Convert.ToInt32(Console.ReadLine());
+        int res = 0;
+        for (int i = x; i > 0; i++)
+        {
+
+            res = res + x % 10;
+            x = x / 10;
+        }
+
+        Console.WriteLine("Сумма чисел равна: " + res);
+    }
+}
