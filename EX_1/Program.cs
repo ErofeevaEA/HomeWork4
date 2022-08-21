8c85f24ec12d0155bbe32173552603ebde717eea
ErofeevaEvgeniya <golubenko.ewgenia@yandex.ru>
2022-08-21 15:20:38 +0300

    Exercise 1

---
+++ EX_1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите 1 число: ");
+        int x = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите 2 число: ");
+        int y = Convert.ToInt32(Console.ReadLine());
+        int res = 1;
+
+        for (int i = 0; i < y; i++)
+        {
+            res = res * x;
+
+        }
+
+        Console.WriteLine(res);
+    }
+}
