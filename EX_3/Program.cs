43bca668d5fddcd4699fbb960e3b49754cbc6090
ErofeevaEvgeniya <golubenko.ewgenia@yandex.ru>
2022-08-21 15:26:15 +0300

    Exercise 3

---
+++ EX_3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+
+    {
+
+        Console.WriteLine("Введите размерность массива:");
+        int i, n = Convert.ToInt32(Console.ReadLine());
+        int[] mas = new int[n];
+        Console.WriteLine("Введите массив:");
+        string[] str = Console.ReadLine().Split(new char[] { ' ', '\n', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+        for (i = 0; i < (n < str.Length ? n : str.Length); ++i)
+            mas[i] = Convert.ToInt32(str[i]);
+        Console.WriteLine("Введенный массив:");
+        Console.Write("[ ");
+
+        for (i = 0; i < n; ++i)
+            Console.Write(mas[i] + ",");
+
+        Console.Write("]");
+        Console.ReadLine();
+    }
+}
